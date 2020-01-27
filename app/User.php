35bde5e2d40c46e54df5ace26009d9573c9dda21
Denis John <denis.john.ndarake@gmail.com>
2020-01-27 10:40:56 +0100

    One to One relationship between the users table and the phones table

--- app/User.php
+++ app/User.php
@@ -2,9 +2,10 @@
 
 namespace App;
 
+use App\Phone;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
@@ -36,4 +37,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function phone()
+    {
+        return $this->hasOne(Phone::class);
+    }
 }
